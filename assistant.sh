3596253641958279b80b3e3152a410a152eb548f
gomezzz3 <39501980+gomezzz3@users.noreply.github.com>
2018-06-21 17:50:05 -0700

    Add files via upload

---
+++ assistant.sh
@@ -0,0 +1,30 @@
+echo "Hello! I'm Rocky, the Personal Assistant, and I'm ready to assist you in your startup for week one of DIGHUM101!"
+
+echo "The date for today is posted below"
+
+Thu Jun 21 15:31:52 PDT 2018
+
+     June 2018
+Su Mo Tu We Th Fr Sa
+                1  2
+ 3  4  5  6  7  8  9
+10 11 12 13 14 15 16
+17 18 19 20 21 22 23
+24 25 26 27 28 29 30
+
+echo "Now lets get started!"
+
+echo "Tip 1, open your terminal and type Jupyter Notebook to open the Jupyter Notebook program"
+echo "Tip 2, click on the directory the says Desktop, then proceed to click on the DIGHUM101 folder"
+echo "Tip 3, click on the direcotry that says Week 1, then click the notebook titled 1-1"
+echo "Tip 4, print out your favorite song quote by using the print function print()" 
+echo "Tip 5, insert your favorite song quote wrapped in quotations in the parenthesis print('quote...')" 
+echo "Tip 6, now lets turn your favorite song quote into a variable by using X = Y method"
+echo "Tip 7, put the variable name on the left, the = symbol in the middle, and the variable definition on the right"
+echo "Tip 8, make sure that the variable name sorresponds with the definition, ex. Beyonce = 'Baby boy you stay on my mind' "
+echo "Tip 9, now that you've defined your variable, press enter to save it
+echo "Tip 10, to print your variable use the print() function and insert your variable name in the parentheis, ex. print(Beyonce)"
+
+printf "n\"
+
+
